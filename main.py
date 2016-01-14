b7e0fb951a91051ed3312ec3daa873993ecaeba9
Simon Wu <amakalf@gmail.com>
2016-01-14 14:21:54 -0800

    part 1 done

--- main.py
+++ main.py
@@ -10,16 +10,27 @@ image = plt.imread('./%s.jpg' % sys.argv[1])/255.
 r_grad, c_grad = np.gradient(np.dot(image, [0.299, 0.587, 0.114]))[:2]
 grad = r_grad**2 + c_grad**2
 
-coords = peak_local_max(grad, min_distance=4, threshold_rel=0,\
-        num_peaks=grad.size/400)
+coords = peak_local_max(grad, min_distance=8, threshold_rel=0)
 coords = np.vstack((coords,[0,0],[0,image.shape[1]],[image.shape[0],0],\
         [image.shape[0],image.shape[1]]))
 tri = Delaunay(coords)
-for triangle in tri.simplices:
-    pts = coords[triangle]
-    rr, cc = polygon(pts[:,0], pts[:,1])
-    image[rr, cc, 0] = np.mean(image[rr, cc, 0])
-    image[rr, cc, 1] = np.mean(image[rr, cc, 1])
-    image[rr, cc, 2] = np.mean(image[rr, cc, 2])
 
-plt.imsave('./%sflat.jpg' % sys.argv[1], image)
+if (sys.argv[2] == 'mean'):
+    for triangle in tri.simplices:
+        pts = coords[triangle]
+        rr, cc = polygon(pts[:,0], pts[:,1])
+        if rr.size:
+            image[rr, cc, 0] = np.mean(image[rr, cc, 0])
+            image[rr, cc, 1] = np.mean(image[rr, cc, 1])
+            image[rr, cc, 2] = np.mean(image[rr, cc, 2])
+
+elif (sys.argv[2] == 'median'):
+    for triangle in tri.simplices:
+        pts = coords[triangle]
+        rr, cc = polygon(pts[:,0], pts[:,1])
+        if rr.size:
+            image[rr, cc, 0] = np.median(image[rr, cc, 0])
+            image[rr, cc, 1] = np.median(image[rr, cc, 1])
+            image[rr, cc, 2] = np.median(image[rr, cc, 2])
+
+plt.imsave('./{0}{1}.jpg'.format(*sys.argv[1:]), image)
